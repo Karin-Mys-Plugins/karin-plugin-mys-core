5263eb0e8368b96c0ef0fcbcb9fccdb220b570a7
bbaban <zasd32345@gmail.com>
2025-12-17 21:50:30 +0800

    style: 优化底部插件信息

--- src/exports/render/layout/DefaultLayout.tsx
+++ src/exports/render/layout/DefaultLayout.tsx
@@ -28,7 +28,7 @@ export const DefaultLayoutComponent: React.FC<DefaultLayoutProps> = ({
     >
       {children}
 
-      <div className='absolute bottom-0 left-0 flex w-full items-center justify-center gap-4 px-4 py-2.5 text-sm'>
+      <div className='absolute bottom-0 left-0 flex w-full items-center justify-center gap-4 px-4 py-2.5 text-sm bg-black/20'>
         {mysPlugin && (
           <>
             <div className='flex items-center gap-2'>
@@ -38,7 +38,7 @@ export const DefaultLayoutComponent: React.FC<DefaultLayoutProps> = ({
               />
               <div className='flex flex-col'>
                 <span className='text-[5px] font-bold leading-none text-black'>KARIN-MYS-PLUGIN</span>
-                <div className='flex items-baseline gap-0.5'>
+                <div className={`flex ${mysPlugin.version.length > 8 ? 'flex-col' : 'items-baseline gap-0.5'}`}>
                   <span className='font-semibold'>{mysPlugin.name}</span>
                   <strong className='text-xs font-bold text-[#f2c06f]'>v{mysPlugin.version}</strong>
                 </div>
@@ -56,7 +56,7 @@ export const DefaultLayoutComponent: React.FC<DefaultLayoutProps> = ({
           />
           <div className='flex flex-col'>
             <span className='text-[5px] font-bold leading-none text-black'>KARIN-PLUGIN</span>
-            <div className='flex items-baseline gap-0.5'>
+            <div className={`flex ${dir.version.length > 8 ? 'flex-col' : 'items-baseline gap-0.5'}`}>
               <span className='font-semibold'>MysCore</span>
               <strong className='text-xs font-bold text-[#f2c06f]'>v{dir.version}</strong>
             </div>
@@ -72,7 +72,7 @@ export const DefaultLayoutComponent: React.FC<DefaultLayoutProps> = ({
           />
           <div className='flex flex-col'>
             <span className='text-[5px] font-bold leading-none text-black'>IS DRIVEN BY</span>
-            <div className='flex items-baseline gap-1.5'>
+            <div className={`flex ${karinVersion.length > 8 ? 'flex-col' : 'items-baseline gap-1.5'}`}>
               <span className='font-semibold'>Karin</span>
               <strong className='text-[10px] font-bold text-[#f2c06f]'>v{karinVersion}</strong>
             </div>
